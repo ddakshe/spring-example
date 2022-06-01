e0a966ac2d88400e36529a39ec53f4718543c82f
Kennen <ktkim@help-me.kr>
2022-06-02 01:35:31 +0900

    오타 수정

--- src/main/java/com/example/spring/api/dto/SaveUserDto.java
+++ src/main/java/com/example/spring/api/dto/SaveUserDto.java
@@ -2,33 +2,17 @@ package com.example.spring.api.dto;
 
 
 import com.example.spring.domain.User;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
-@Data
+@NoArgsConstructor
+@Getter
 public class SaveUserDto {
     private String name;
-    private String family;
-    private Boolean korean;
     private Integer age;
 
     public User toEntity() {
         return User.builder().name(name).age(age).build();
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setFamily(String family) {
-        this.family = family;
-    }
-
-    public void setKorean(Boolean korean) {
-        this.korean = korean;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
     }
 }
